2c5192e1a0a0b0b4bd45c6450c2fd47c09955483
Berthonise Charles <berthonisecharles@Berthonises-MacBook-Air-2.local>
2022-02-16 10:34:03 -0500

    fb

---
+++ src/scenes/Signup.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Signup() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const handleFormSumbit = (event) => {
+        event.preventDefault();
+        alert(`Trying to sign-up as ${email}`);
+    }
+    return (
+        <>
+<h1>Signup</h1>
+<hr />
+<form onSubmit={handleFormSumbit}>
+    <label>Email: <input type="email" value={email} onChange={e => setEmail(e.target.value)}/></label>
+    <br/>
+    <label>Password: <input type="password" value={password} onChange={e => setPassword(e.target.value)} /></label>
+    <br/>
+    <input type='submit' value='Signup' />
+</form>
+<p>Already a user?<Link to='/login'>Login</Link></p>
+</>
+    )
+    
+}
